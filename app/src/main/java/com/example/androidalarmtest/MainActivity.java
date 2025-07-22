223bb567f9dedc89a6f1dbf89f76009930932f67
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-07-22 11:41:27 +0900

    add: 리스트뷰 토스트 팝업 작업

--- app/src/main/java/com/example/androidalarmtest/MainActivity.java
+++ app/src/main/java/com/example/androidalarmtest/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.androidalarmtest;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,10 +23,30 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //add
+        Button listButton = (Button) findViewById(R.id.listButton);
+        listButton.setOnClickListener(new View.OnClickListener() {  //리스트 버튼 클릭
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("리스트");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {  //개별 클릭 시 토스트 띄움(별도로 띄움)
+                        Toast.makeText(getApplicationContext(), items[i], Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+
+            }
+        });
+        return insets;
         });
     } //end main
 
-    Button listButton = (Button) findViewById(R.id.listButton);
+
 
 }
